a06dd7516458677ebf90bb980223ad7198fe9ee0
f1rpo <no.m@il.specified>
2022-06-29 21:33:48 +0200

    Refactor SelfMod class (based on Taurus 1.00)
    In the process of letting Taurus bypass savegame compatibility checks in the EXE, I've moved (fallback) code for calculating virtual memory offsets into the base class and improved error reporting a bit. [advc.092b]

--- CvGameCoreDLL/SelfMod.h
+++ CvGameCoreDLL/SelfMod.h
@@ -1,24 +1,26 @@
+// advc.092b: Let's put all self-modifying code in a single place
 #pragma once
 #ifndef SELF_MOD_H
 #define SELF_MOD_H
 
-/*	advc.092b: Let's put all runtime patches for Civ4BeyondSword.exe (v3.19)
-	in one place. */
-
-
-
-class Civ4BeyondSwordMods
+// Runtime modifications to the native code of Civ4BeyondSword.exe (v3.19)
+class Civ4BeyondSwordPatches
 {
 public:
+	void patchPlotIndicatorSize(); // (exposed to Python via CyGameCoreUtils) 
 	bool isPlotIndicatorSizePatched() const { return m_bPlotIndicatorSizePatched; }
-	void patchPlotIndicatorSize(); // (exposed to Python via CyGameCoreUtils)
+	Civ4BeyondSwordPatches()
+	:	m_bPlotIndicatorSizePatched(false)
+	{}
+
 private:
 	bool m_bPlotIndicatorSizePatched;
+	void showErrorMsgToPlayer(CvWString szMsg);
 };
 
 namespace smc
 {
-	extern Civ4BeyondSwordMods BtS_EXE;
-};
+	extern Civ4BeyondSwordPatches BtS_EXE;
+}
 
 #endif
