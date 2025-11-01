ea1206ef268c1a3554859c0032d33ad952e9cef3
XANA <206968445+SuperXANA@users.noreply.github.com>
2025-10-31 23:11:37 -0400

    Add XML information for the base AdvCiv war utility AI data, which are used to allow AI leaders to make decisions related to war and peace.
    I also set iAIWeight to integer value 100 by default in the DLL.

--- CvGameCoreDLL/WarUtilityAspectAI.cpp
+++ CvGameCoreDLL/WarUtilityAspectAI.cpp
@@ -15,7 +15,7 @@ bool CvWarUtilityAspectAIInfo::read(CvXMLLoadUtility* pXML)
 	if (!base_t::read(pXML))
 		return false;
 
-	pXML->GetChildXmlValByName(&m_iAIWeight, "iAIWeight");
+	pXML->GetChildXmlValByName(&m_iAIWeight, "iAIWeight", 100);
 	return true;
 }
 // XANA: 11-01-2025 Leader Specific War Utility Aspect Weights
