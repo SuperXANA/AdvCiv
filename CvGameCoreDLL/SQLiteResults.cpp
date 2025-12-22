a4110011e0e4aa0647936d2883d4fa0fde7e6ddc
XANA <206968445+SuperXANA@users.noreply.github.com>
2025-12-21 19:31:40 -0500

    Slight refactoring of database C++ code

---
+++ CvGameCoreDLL/SQLiteResults.cpp
@@ -0,0 +1,53 @@
+#include "SQLiteResults.h"
+
+SQLiteResults::SQLiteResults(CvSQLStatement& stmt) 
+	: m_statement(stmt), m_bValid(true) 
+	{}
+
+bool SQLiteResults::next()
+{
+	m_bValid = m_statement.step();
+	return m_bValid;
+}
+
+int SQLiteResults::getInt(const char* szColName) const
+{
+	int const idx = getColumnIndex(szColName);
+	return (idx >= 0) ? getInt(idx) : 0;
+}
+
+scaled SQLiteResults::getFloat(const char* szColName) const
+{
+	int const idx = getColumnIndex(szColName);
+	return (idx >= 0) ? getFloat(idx) : fixp(0.0f);
+}
+
+scaled SQLiteResults::getScaled(const char* szColName) const
+{
+	int const idx = getColumnIndex(szColName);
+	return (idx >= 0) ? getScaled(idx) : 0;
+}
+
+bool SQLiteResults::getBool(const char* szColName) const
+{
+	int const idx = getColumnIndex(szColName);
+	return (idx >= 0) ? getBool(idx) : false;
+}
+
+CvString SQLiteResults::getString(const char* szColName) const
+{
+	int const idx = getColumnIndex(szColName);
+	return (idx >= 0) ? getString(idx) : CvString();
+}
+
+bool SQLiteResults::isNull(const char* szColName) const
+{
+	int const idx = getColumnIndex(szColName);
+	return (idx >= 0) ? isNull(idx) : false;
+}
+
+int SQLiteResults::getColumnType(const char* szColName) const
+{
+	int const idx = getColumnIndex(szColName);
+	return (idx >= 0) ? getColumnType(idx) : SQLITE_NULL;
+}
---
+++ CvGameCoreDLL/SQLiteResults.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#include "CvGameCoreDLL.h"
+class SQLiteResults : private boost::noncopyable
+{
+public:
+	explicit SQLiteResults(SQLiteStatement& stmt);
+	
+	bool next();
+	
+	int getInt(const char* szColName) const;
+	scaled getFloat(const char* szColName) const;
+	scaled getScaled(const char* szColName) const;
+	bool getBool(const char* szColName) const;
+	CvString getString(const char* szColName) const;
+	bool isNull(const char* szColName) const;
+	int getColumnType(const char* szColName) const;
+	
+private:
+	SQLiteStatement& m_statement;
+	bool m_bValid;
+	
+	int getColumnIndex(const char* szName) const
+	{
+		if (!m_statement)
+		{
+			return -1;
+		}
+		int count = m_statement.getColumnCount();
+		for (int i = 0; i < count; ++i)
+		{
+			const char* szColName = m_statement.getColumnName(i);
+			if (szColName && std::strcmp(szColName, szName) == 0)
+				return i;
+		}
+		return -1;
+	}
+	int getColumnType(int iColumn) const
+	{
+		return m_statement ? sqlite3_column_type(m_statement.getHandle(), col) : SQLITE_NULL;
+	}
+	int getInt(int iColumn) const
+	{
+		return m_statement ? sqlite3_column_int(m_statement.getHandle(), col) : 0;
+	}
+	scaled getFloat(int iColumn) const
+	{
+		return m_statement ? fixp(static_cast<float>(sqlite3_column_double(m_statement.getHandle(), col))) ; scaled();
+	}
+	double getDouble(int iColumn) const
+	{
+		return m_statement ? sqlite3_column_double(m_statement.getHandle(), col) : 0d;
+	}
+	scaled getScaled(int iColumn) const
+	{
+		return m_statement ? scaled(sqlite3_column_double(m_statement.getHandle(), col)) : scaled();
+	}
+	bool getBool(int iColumn) const
+	{
+		return m_statement ? sqlite3_column_int(m_statement.getHandle(), col) != 0 : false;
+	}
+	const char* getText(int iColumn) const
+	{
+		return m_statement ? reinterpret_cast<const char*>(sqlite3_column_text(m_statement.getHandle(), col)) : "";
+	}
+	CvString getString(int iColumn) const
+	{
+		const char* txt = getText(col);
+		return txt ? CvString(txt) : CvString();
+	}
+	bool isNull(int iColumn) const
+	{
+		return m_statement ? sqlite3_column_type(m_statement.getHandle(), col) == SQLITE_NULL : false;
+	}
+};
