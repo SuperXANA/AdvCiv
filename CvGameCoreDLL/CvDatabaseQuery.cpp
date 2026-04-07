86fc7a7a44aeea959bd8e10dc3f762d98cb8c034
XANA <206968445+SuperXANA@users.noreply.github.com>
2026-04-06 22:55:45 -0400

    Add interface class for constructing SQL strings automatically rather than manually formatting SQL query strings
    Example usage:
    CvDatabaseQuery query;
    query.select("Type, Cost, bNaval")
         .from("Units")
         .where("Era", "<=", ":era")        // Uses <= operator
         .andWhere("bEnabled", "=", "1");    // Adds AND clause
    
    The class automatically constructs the same SQL string as manually typing "SELECT Type, Cost, bNaval FROM Units WHERE Era <= :era AND bEnabled = 1" into the SQLiteStatement constructor. Once the SQL query string has been successfully created, a Statement object prepared with that query can be returned via the toStatement function.

---
+++ CvGameCoreDLL/CvDatabaseQuery.cpp
@@ -0,0 +1,323 @@
+#include "CvDatabaseQuery.h"
+#include "SQLiteStatement.h"
+
+CvDatabaseQuery::CvDatabaseQuery() 
+	: m_hasWhere(false), m_hasSet(false), m_hasColumns(false) 
+{}
+
+CvDatabaseQuery::CvDatabaseQuery(const char* tableName)
+	: m_hasWhere(false), m_hasSet(false), m_hasColumns(false)
+{
+	m_sql = CvString::format("SELECT * FROM ");
+	m_sql += CvString::format(tableName);
+}
+
+CvDatabaseQuery& CvDatabaseQuery::select(const char* columns)
+{
+	m_sql = CvString::format("SELECT ");
+	m_sql += CvString::format(columns);
+	m_hasWhere = false;
+	m_hasSet = false;
+	m_hasColumns = false;
+	return *this;
+}
+
+CvDatabaseQuery& CvDatabaseQuery::selectDistinct(const char* columns)
+{
+	m_sql = CvString::format("SELECT DISTINCT ");
+	m_sql += CvString::format(columns);
+	m_hasWhere = false;
+	m_hasSet = false;
+	m_hasColumns = false;
+	return *this;
+}
+
+CvDatabaseQuery& CvDatabaseQuery::from(const char* table)
+{
+	m_sql += CvString::format(" FROM ");
+	m_sql += CvString::format(table);
+	return *this;
+}
+
+CvDatabaseQuery& CvDatabaseQuery::join(const char* table, const char* onCondition)
+{
+	m_sql += CvString::format(" JOIN ");
+	m_sql += CvString::format(table);
+	m_sql += CvString::format(" ON ");
+	m_sql += CvString::format(onCondition);
+	return *this;
+}
+
+CvDatabaseQuery& CvDatabaseQuery::leftJoin(const char* table, const char* onCondition)
+{
+	m_sql += CvString::format(" LEFT JOIN ");
+	m_sql += CvString::format(table);
+	m_sql += CvString::format(" ON ");
+	m_sql += CvString::format(onCondition);
+	return *this;
+}
+
+CvDatabaseQuery& CvDatabaseQuery::innerJoin(const char* table, const char* onCondition)
+{
+	m_sql += CvString::format(" INNER JOIN ");
+	m_sql += CvString::format(table);
+	m_sql += CvString::format(" ON ");
+	m_sql += CvString::format(onCondition);
+	return *this;
+}
+
+CvDatabaseQuery& CvDatabaseQuery::where(const char* column, const char* op, const char* paramName)
+{
+	m_sql += CvString::format(" WHERE ");
+	m_sql += CvString::format(column);
+	m_sql += CvString::format(" ");
+	m_sql += CvString::format(op);
+	m_sql += CvString::format(" ");
+	m_sql += CvString::format(paramName);
+	m_hasWhere = true;
+	return *this;
+}
+
+CvDatabaseQuery& CvDatabaseQuery::andWhere(const char* column, const char* op, const char* paramName)
+{
+	if (!m_hasWhere)
+	{
+		return where(column, op, paramName);
+	}
+	m_sql += CvString::format(" AND ");
+	m_sql += CvString::format(column);
+	m_sql += CvString::format(" ");
+	m_sql += CvString::format(op);
+	m_sql += CvString::format(" ");
+	m_sql += CvString::format(paramName);
+	return *this;
+}
+
+CvDatabaseQuery& CvDatabaseQuery::orWhere(const char* column, const char* op, const char* paramName)
+{
+	if (!m_hasWhere)
+	{
+		return where(column, op, paramName);
+	}
+	m_sql += CvString::format(" OR ");
+	m_sql += CvString::format(column);
+	m_sql += CvString::format(" ");
+	m_sql += CvString::format(op);
+	m_sql += CvString::format(" ");
+	m_sql += CvString::format(paramName);
+	return *this;
+}
+
+CvDatabaseQuery& CvDatabaseQuery::whereEquals(const char* column, const char* paramName)
+{
+	return where(column, "=", paramName);
+}
+
+CvDatabaseQuery& CvDatabaseQuery::whereNotEquals(const char* column, const char* paramName)
+{
+	return where(column, "!=", paramName);
+}
+
+CvDatabaseQuery& CvDatabaseQuery::whereLessThan(const char* column, const char* paramName)
+{
+	return where(column, "<", paramName);
+}
+
+CvDatabaseQuery& CvDatabaseQuery::whereLessThanOrEqual(const char* column, const char* paramName)
+{
+	return where(column, "<=", paramName);
+}
+
+CvDatabaseQuery& CvDatabaseQuery::whereGreaterThan(const char* column, const char* paramName)
+{
+	return where(column, ">", paramName);
+}
+
+CvDatabaseQuery& CvDatabaseQuery::whereGreaterThanOrEqual(const char* column, const char* paramName)
+{
+	return where(column, ">=", paramName);
+}
+
+CvDatabaseQuery& CvDatabaseQuery::whereLike(const char* column, const char* paramName)
+{
+	return where(column, "LIKE", paramName);
+}
+
+CvDatabaseQuery& CvDatabaseQuery::whereNull(const char* column)
+{
+	m_sql += CvString::format(" WHERE ");
+	m_sql += CvString::format(column);
+	m_sql += CvString::format(" IS NULL");
+	m_hasWhere = true;
+	return *this;
+}
+
+CvDatabaseQuery& CvDatabaseQuery::whereNotNull(const char* column)
+{
+	m_sql += CvString::format(" WHERE ");
+	m_sql += CvString::format(column);
+	m_sql += CvString::format(" IS NOT NULL");
+	m_hasWhere = true;
+	return *this;
+}
+
+CvDatabaseQuery& CvDatabaseQuery::whereRaw(const char* condition)
+{
+	m_sql += CvString::format(" WHERE ");
+	m_sql += CvString::format(condition);
+	m_hasWhere = true;
+	return *this;
+}
+
+CvDatabaseQuery& CvDatabaseQuery::andWhereRaw(const char* condition)
+{
+	if (!m_hasWhere)
+		return whereRaw(condition);
+	m_sql += CvString::format(" AND ");
+	m_sql += CvString::format(condition);
+	return *this;
+}
+
+CvDatabaseQuery& CvDatabaseQuery::orWhereRaw(const char* condition)
+{
+	if (!m_hasWhere)
+		return whereRaw(condition);
+	m_sql += CvString::format(" OR ");
+	m_sql += CvString::format(condition);
+	return *this;
+}
+
+CvDatabaseQuery& CvDatabaseQuery::groupBy(const char* columns)
+{
+	m_sql += CvString::format(" GROUP BY ");
+	m_sql += CvString::format(columns);
+	return *this;
+}
+
+CvDatabaseQuery& CvDatabaseQuery::having(const char* condition)
+{
+	m_sql += CvString::format(" HAVING ");
+	m_sql += CvString::format(condition);
+	return *this;
+}
+
+CvDatabaseQuery& CvDatabaseQuery::orderBy(const char* column, bool ascending)
+{
+	m_sql += CvString::format(" ORDER BY ");
+	m_sql += CvString::format(column);
+	if (ascending)
+		m_sql += CvString::format(" ASC");
+	else
+		m_sql += CvString::format(" DESC");
+	return *this;
+}
+
+CvDatabaseQuery& CvDatabaseQuery::limit(int count)
+{
+	m_sql += CvString::format(" LIMIT ");
+	m_sql += CvString::format(intToString(count).c_str());
+	return *this;
+}
+
+CvDatabaseQuery& CvDatabaseQuery::offset(int count)
+{
+	m_sql += CvString::format(" OFFSET ");
+	m_sql += CvString::format(intToString(count).c_str());
+	return *this;
+}
+
+CvDatabaseQuery& CvDatabaseQuery::update(const char* table)
+{
+	m_sql = CvString::format("UPDATE ");
+	m_sql += CvString::format(table);
+	m_hasWhere = false;
+	m_hasSet = false;
+	return *this;
+}
+
+CvDatabaseQuery& CvDatabaseQuery::set(const char* column, const char* paramName)
+{
+	m_sql += CvString::format(" SET ");
+	m_sql += CvString::format(column);
+	m_sql += CvString::format(" = ");
+	m_sql += CvString::format(paramName);
+	m_hasSet = true;
+	return *this;
+}
+
+CvDatabaseQuery& CvDatabaseQuery::addSet(const char* column, const char* paramName)
+{
+	if (!m_hasSet)
+		return set(column, paramName);
+	m_sql += CvString::format(", ");
+	m_sql += CvString::format(column);
+	m_sql += CvString::format(" = ");
+	m_sql += CvString::format(paramName);
+	return *this;
+}
+
+CvDatabaseQuery& CvDatabaseQuery::insertInto(const char* table)
+{
+	m_sql = CvString::format("INSERT INTO ");
+	m_sql += CvString::format(table);
+	m_hasWhere = false;
+	m_hasSet = false;
+	m_hasColumns = false;
+	return *this;
+}
+
+CvDatabaseQuery& CvDatabaseQuery::columns(const char* columnList)
+{
+	m_sql += CvString::format(" (");
+	m_sql += CvString::format(columnList);
+	m_sql += CvString::format(")");
+	m_hasColumns = true;
+	return *this;
+}
+
+CvDatabaseQuery& CvDatabaseQuery::values(const char* paramList)
+{
+	m_sql += CvString::format(" VALUES (");
+	m_sql += CvString::format(paramList);
+	m_sql += CvString::format(")");
+	return *this;
+}
+
+CvDatabaseQuery& CvDatabaseQuery::deleteFrom(const char* table)
+{
+	m_sql = CvString::format("DELETE FROM ");
+	m_sql += CvString::format(table);
+	m_hasWhere = false;
+	m_hasSet = false;
+	return *this;
+}
+
+void CvDatabaseQuery::clear()
+{
+	m_sql.clear();
+	m_hasWhere = false;
+	m_hasSet = false;
+	m_hasColumns = false;
+}
+
+const CvString& CvDatabaseQuery::getSQLString() const
+{
+    return m_sql;
+}
+
+SQLiteStatement CvDatabaseQuery::toStatement() const
+{
+    return SQLiteStatement(m_sql);
+}
+
+void CvDatabaseQuery::applyTo(SQLiteStatement& stmt) const
+{
+    stmt.prepare(m_sql);
+}
+
+std::string CvDatabaseQuery::intToString(int value) const
+{
+	std::ostringstream kStream;
+	kStream << value;
+    return kStream.str();
+}
---
+++ CvGameCoreDLL/CvDatabaseQuery.h
@@ -0,0 +1,72 @@
+#pragma once
+
+#include "CvGameCoreDLL.h"
+
+#ifndef CV_SQLITEQUERY_H
+#define CV_SQLITEQUERY_H
+
+class SQLiteStatement;
+
+class CvDatabaseQuery : private boost::noncopyable
+{
+public:
+	CvDatabaseQuery();
+	explicit CvDatabaseQuery(const char* tableName);
+	~CvDatabaseQuery() {};
+
+	CvDatabaseQuery& select(const char* columns);
+	CvDatabaseQuery& selectDistinct(const char* columns);
+	CvDatabaseQuery& from(const char* table);
+	
+	CvDatabaseQuery& join(const char* table, const char* onCondition);
+	CvDatabaseQuery& leftJoin(const char* table, const char* onCondition);
+	CvDatabaseQuery& innerJoin(const char* table, const char* onCondition);
+	
+	CvDatabaseQuery& where(const char* column, const char* op, const char* paramName);
+	CvDatabaseQuery& andWhere(const char* column, const char* op, const char* paramName);
+	CvDatabaseQuery& orWhere(const char* column, const char* op, const char* paramName);
+	
+	CvDatabaseQuery& whereEquals(const char* column, const char* paramName);
+	CvDatabaseQuery& whereNotEquals(const char* column, const char* paramName);
+	CvDatabaseQuery& whereLessThan(const char* column, const char* paramName);
+	CvDatabaseQuery& whereLessThanOrEqual(const char* column, const char* paramName);
+	CvDatabaseQuery& whereGreaterThan(const char* column, const char* paramName);
+	CvDatabaseQuery& whereGreaterThanOrEqual(const char* column, const char* paramName);
+	CvDatabaseQuery& whereLike(const char* column, const char* paramName);
+	CvDatabaseQuery& whereNotNull(const char* column);
+	CvDatabaseQuery& whereNull(const char* column);
+	
+	CvDatabaseQuery& whereRaw(const char* condition);
+	CvDatabaseQuery& andWhereRaw(const char* condition);
+	CvDatabaseQuery& orWhereRaw(const char* condition);
+	
+	CvDatabaseQuery& groupBy(const char* columns);
+	CvDatabaseQuery& having(const char* condition);
+	CvDatabaseQuery& orderBy(const char* column, bool ascending = true);
+	CvDatabaseQuery& limit(int count);
+	CvDatabaseQuery& offset(int count);
+	
+	CvDatabaseQuery& update(const char* table);
+	CvDatabaseQuery& set(const char* column, const char* paramName);
+	CvDatabaseQuery& addSet(const char* column, const char* paramName);
+	
+	CvDatabaseQuery& insertInto(const char* table);
+	CvDatabaseQuery& columns(const char* columnList);
+	CvDatabaseQuery& values(const char* paramList);
+	
+	CvDatabaseQuery& deleteFrom(const char* table);
+	
+	void clear();
+	const CvString& getSQLString() const;
+	
+	SQLiteStatement toStatement() const;
+	void applyTo(SQLiteStatement& stmt) const;
+
+private:
+	CvString m_sql;
+	bool m_hasWhere;
+	bool m_hasSet;
+	bool m_hasColumns;
+	static std::string intToString(int value) const;
+};
+#endif // CV_SQLITEQUERY_H
