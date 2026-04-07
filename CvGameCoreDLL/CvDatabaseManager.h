87f04569d52761ca30d804cb16eef937dd2f0fed
XANA <206968445+SuperXANA@users.noreply.github.com>
2026-04-07 19:10:02 -0400

    Add the missing forward declarations for the sqlite structs/classes required by the Manager class

--- CvGameCoreDLL/CvDatabaseManager.h
+++ CvGameCoreDLL/CvDatabaseManager.h
@@ -5,6 +5,9 @@
 #ifndef CV_DATABASE_H
 #define CV_DATABASE_H
 
+struct sqlite3;
+class SQLiteConnection;
+
 class CvDatabaseManager : private boost::noncopyable
 friend class SQLiteStatement; // XANA (note): The Statement class is the only one needing access to the sqlite pointer. For safety we will hide the sqlite pointer from the rest of the normal game code.
 {
