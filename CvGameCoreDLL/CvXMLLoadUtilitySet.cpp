0113d757b00442ca87031d5f726bdf3b039ab0c7
XANA <206968445+SuperXANA@users.noreply.github.com>
2026-04-27 00:03:42 -0400

    Disable XML caching mechanism for Race class data objects since we can't create custom caching mechanisms without breaking the EXE

--- CvGameCoreDLL/CvXMLLoadUtilitySet.cpp
+++ CvGameCoreDLL/CvXMLLoadUtilitySet.cpp
@@ -667,7 +667,7 @@ bool CvXMLLoadUtility::LoadPreMenuGlobals()
 	LoadGlobalClassInfo(GC.m_paAnimationCategoryInfo, "CIV4AnimationInfos", "Units", "Civ4AnimationInfos/AnimationCategories/AnimationCategory", false);
 	LoadGlobalClassInfo(GC.m_paAnimationPathInfo, "CIV4AnimationPathInfos", "Units", "Civ4AnimationPathInfos/AnimationPaths/AnimationPath", false);
 // XANA: 06-21-2025 Racial Marks
-	LoadGlobalClassInfo(GC.m_paRaceInfo, "CIV4RaceInfos", "Civilizations", "Civ4RaceInfos/RaceInfos/RaceInfo", true, &CvDLLUtilityIFaceBase::createRaceInfoCacheObject);
+	LoadGlobalClassInfo(GC.m_paRaceInfo, "CIV4RaceInfos", "Civilizations", "Civ4RaceInfos/RaceInfos/RaceInfo", true);
 // XANA: 06-21-2025 Racial Marks
 	LoadGlobalClassInfo(GC.m_paPromotionInfo, "CIV4PromotionInfos", "Units", "Civ4PromotionInfos/PromotionInfos/PromotionInfo", true, &CvDLLUtilityIFaceBase::createPromotionInfoCacheObject);
 	LoadGlobalClassInfo(GC.m_paTraitInfo, "CIV4TraitInfos", "Civilizations", "Civ4TraitInfos/TraitInfos/TraitInfo", false);
