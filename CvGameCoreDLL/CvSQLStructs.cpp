457d297ae1a340b4172a39e29081aa50c34fe9ee
XANA <206968445+SuperXANA@users.noreply.github.com>
2026-04-29 17:38:07 -0400

    Move the Civ4 SQLite data structures to their own code files

---
+++ CvGameCoreDLL/CvSQLStructs.cpp
@@ -0,0 +1,7 @@
+// XANA: 10-04-2025 Data Storage Interface for Advanced Civ
+#include "CvGameCoreDLL.h"
+#include "CvSQLStructs.h"
+#include "CvDatabaseManager.h"
+#include "CvDatabaseQuery.h" // XANA (note): Imports SQLiteStatement.h
+#include "SQLiteTransaction.h"
+// XANA: 10-04-2025 Data Storage Interface for Advanced Civ
---
+++ CvGameCoreDLL/CvSQLStructs.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifndef CVSQLSTRUCTS_H
+#define CVSQLSTRUCTS_H
+
+class CvDatabaseManager;
+class CvDatabaseQuery;
+class SQLiteStatement;
+class SQLiteTransaction;
+
+#endif	// CVSQLSTRUCTS_H
