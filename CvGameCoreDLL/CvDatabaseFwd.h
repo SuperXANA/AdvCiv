9b1c6dced11a1a53d400dc3267d79400404e8934
XANA <206968445+SuperXANA@users.noreply.github.com>
2026-04-30 23:06:15 -0400

    Suppress compilation warnings for trusted, locked sqlite source code version 3.6.6.2

--- CvGameCoreDLL/CvDatabaseFwd.h
+++ CvGameCoreDLL/CvDatabaseFwd.h
@@ -3,9 +3,17 @@
 #ifndef CV_SQLITEFWD_H
 #define CV_SQLITEFWD_H
 
+// Silence specific MSVC 7.1 warnings for this code block during inclusion
+#pragma warning(push)
+#pragma warning(disable: 4100) // Unreferenced formal parameter
+#pragma warning(disable: 4244) // Conversion, possible loss of data
+#pragma warning(disable: 4018) // Signed/unsigned mismatch
+
 extern "C"
 {
 	#include "sqlite3.h"
 }
+
+#pragma warning(pop) // Restore original Civ4 mod warning levels
 
 #endif
