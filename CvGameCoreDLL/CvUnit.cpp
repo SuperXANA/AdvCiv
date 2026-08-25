SuperXANA/AdvCiv

commit 177701cd0a0f4a295a59b4c7bf2d1c8143853763

Allow vassal/ master to help suppress 3rd-party revolts
change id [advc.184]

Proposed here (toward the end of the post):
https://forums.civfanatics.com/threads/advanced-civ.614217/post-16503765

CvGameCoreDLL/CvUnit.cpp
@@ -7100,10 +7100,20 @@ bool CvUnit::isNoUnitCapture() const
 
 bool CvUnit::isMilitaryHappiness() const
 {
-	return m_pUnitInfo->isMilitaryHappiness() &&
-			/* <advc.184> */ getPlot().isCity() &&
-			(getPlot().getTeam() == getTeam() ||
-			getPlot().getTeam() == GET_TEAM(getTeam()).getMasterTeam()); // </advc.184>
+	return (m_pUnitInfo->isMilitaryHappiness() &&
+			isGarrisonInTeamCity()); // advc.184
+}
+
+// advc.184: Whether it is available for suppressing unhappiness and revolts
+bool CvUnit::isGarrisonInTeamCity() const
+{
+	if (!getPlot().isCity())
+		return false;
+	CvTeam const& kCityTeam = GET_TEAM(getPlot().getTeam());
+	if (isInvisible(kCityTeam.getID(), false))
+		return false;
+	return (kCityTeam.getMasterTeam() == getTeam() ||
+			kCityTeam.getID() == GET_TEAM(getTeam()).getMasterTeam());
 }
 
 /*	advc.101: Replacing iCultureGarrison in XML, which increases too slowly

CvGameCoreDLL/CvCity.cpp
@@ -4278,8 +4278,12 @@ int CvCity::cultureGarrison(PlayerTypes ePlayer) const
 	int iGarrison = 0; // was 1
 	FOR_EACH_UNIT_IN(pUnit, getPlot())
 	{
-		if (pUnit->getTeam() == getTeam()) // advc.184: Had been counting all units
+		// <advc.184> Had been counting all units
+		if (pUnit->getTeam() != TEAMID(ePlayer) &&
+			pUnit->isGarrisonInTeamCity()) // </advc.184>
+		{
 			iGarrison += pUnit->garrisonStrength();
+		}
 	}
 	/*if (atWar(TEAMID(ePlayer), getTeam()))
 		iGarrison *= 2;*/ // advc.023: commented out

CvGameCoreDLL/CvUnit.h
@@ -392,6 +392,7 @@ public:
 		return m_pUnitInfo->isRivalTerritory();
 	}
 	bool isMilitaryHappiness() const;																		// Exposed to Python
+	bool isGarrisonInTeamCity() const; // advc.184
 	int garrisonStrength() const; // advc.101
 	bool isInvestigate() const																				// Exposed to Python
 	{

commit f5a72f766c00b22ee6f876936f2de98011e042c3

Let city owner see other teams' cargo in port
(For once, not adopted from SAS; this comes from a failed assertion on my end.)

Mostly as a fix for an issue introduced by commit 177701cd (vassal/ master units can serve for revolt suppression and military happiness): Since units of a vassal or master are not of the same team, they were invisible to the city owner when in cargo. This had excluded them from all garrison duties. (And when walking out of the city, the cached number of military happiness units became inconsistent.) Arguably, all units in cargo should be unavailable for revolts/ military happiness - because they can't actually defend the city in that state. But the easier fix, for me, is to make all (human) cargo in port visible to the city owner, which also seems fairly logical to me. Note that in-cargo state already didn't affect revolt suppression and military happiness in the base game. [advc.184]

CvGameCoreDLL/CvUnit.cpp
@@ -7125,6 +7125,8 @@ bool CvUnit::isGarrisonInTeamCity() const
 	if (!getPlot().isCity())
 		return false;
 	CvTeam const& kCityTeam = GET_TEAM(getPlot().getTeam());
+	/*	Note that I've changed the bCheckCargo effect in isInvisible so that
+		the city team can see what cargo is in port */
 	if (isInvisible(kCityTeam.getID(), false))
 		return false;
 	return (kCityTeam.getID() == getTeam() ||
@@ -8229,8 +8231,12 @@ bool CvUnit::isInvisible(TeamTypes eTeam, bool bDebug, bool bCheckCargo) const
 		return false;
 	if (alwaysInvisible())
 		return true;
-	if (bCheckCargo && isCargo())
+	if (bCheckCargo && isCargo() &&
+		// advc.184: Not invisible when in port in eTeam's city
+		(!getPlot().isCity() || getPlot().getPlotCity()->getTeam() != eTeam))
+	{
 		return true;
+	}
 	if (getInvisibleType() == NO_INVISIBLE)
 		return false;
 	return !getPlot().isInvisibleVisible(eTeam, getInvisibleType());